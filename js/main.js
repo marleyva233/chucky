b1e112b995af5f0ebabb567218cbeeb65277c7f3
Maribel <marleyva233@gmail.com>
2019-06-25 14:32:25 -0700

    adding in boxes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var answers = {
+	answer1: "fiffany",
+	answer2: "goodguys",
+	answer3: "knife",
+	answer4: "serialkiller",
+	answer5: "andy",
+	answer6: "daftpunk",
+	answer7: "childsplay",
+	answer8: "glen",
+	answer9: "artificialintelligence",
+	answer10: "blue"
+}
