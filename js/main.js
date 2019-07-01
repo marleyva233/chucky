11eda400b03f29420881222f753ab0402e5870fc
Maribel <marleyva233@gmail.com>
2019-07-01 14:15:10 -0700

    added ids to input boxes

--- js/main.js
+++ js/main.js
@@ -11,11 +11,45 @@ var answers = {
 	answer10: "blue"
 }
 function checkPuzzle() {
-	// check that the array matches the answers
-	// if they do not match highlight or  ouput a message
-}
-
-function returnWord(id){
-	return document.getElementById(id);
-	// gather input fields and their values then concat together and pass to an array
+	var question1;
+	var question2;
+	var question3;
+	var question4;
+	var question5;
+	var question6;
+	var question7;
+	var question8;
+	var question9;
+	var question10;
+	for (var i = 1; i < 7; i++) {
+		question1 += document.getElementById("q1i" + i).value;
+	}
+	for (var i = 1; i < 8; i++) {
+		question2 += document.getElementById("q4i" + i).value;
+	}
+	for (var i = 1; i < 5; i++) {
+		question3 += document.getElementById("q4i" + i).value;
+	}
+	for (var i = 1; i < 12; i++) {
+		question4 += document.getElementById("q4i" + i).value;
+	}
+	for (var i = 1; i < 4; i++) {
+		question5 += document.getElementById("q4i" + i).value;
+	}
+	for (var i = 1; i < 8; i++) {
+		question6 += document.getElementById("q4i" + i).value;
+	}
+	for (var i = 1; i < 10; i++) {
+		question7 += document.getElementById("q4i" + i).value;
+	}
+	for (var i = 1; i < 4; i++) {
+		question8 += document.getElementById("q4i" + i).value;
+	}
+	for (var i = 1; i < 22; i++) {
+		question9 += document.getElementById("q9i" + i).value;
+	}
+	for (var i = 1; i < 4; i++) {
+		question10 += document.getElementById("q4i" + i).value;
+	}
+	console.log(question9);
 }
